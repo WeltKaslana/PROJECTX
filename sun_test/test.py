31119cb196288c6aa3c8b981f26156c9a7c00a0b
WeltKaslana <A1530991081@163.com>
2025-06-30 16:25:11 +0800

    new

---
+++ sun_test/test.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from langchain_deepseek import ChatDeepSeek
+
+load_dotenv()
+
+
+print("Hello from hellochain!")
+key = os.getenv("SILICONFLOW_API_KEY")
+base = os.getenv("SILICONFLOW_API_BASE")
+print(f"KEY: {key}")
+model = ChatDeepSeek(
+    model='deepseek-ai/DeepSeek-R1-0528-Qwen3-8B',
+    temperature=0,
+    api_key=key,
+    api_base=base
+)
+res = model.invoke("introduce yourself in Chinese briefly")
+print(res)
